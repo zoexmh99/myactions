a0393b84efffea48b7a221647fae4e2a57e622da
lixiansheng828 <87464542+lixiansheng828@users.noreply.github.com>
2022-05-28 16:21:16 +0800

    Update write.py

--- write.py
+++ write.py
@@ -3,12 +3,11 @@ import random
 import time
 import requests
 import re
-import os,sys
+import os
 
 txt="hah"
-path=sys.path[0]+r'/server1.txt'
 fn=open(path,"w+",encoding="utf-8")
-print("哈哈"+fn.readline())
+print("/server1.txt"+fn.readline())
 
 fn.write(txt)
 
