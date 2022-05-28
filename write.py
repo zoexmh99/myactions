9cfa1c0d9342a591c16e497517088deb136c2ce8
lixiansheng828 <87464542+lixiansheng828@users.noreply.github.com>
2022-05-28 16:43:01 +0800

    Update write.py

--- write.py
+++ write.py
@@ -5,7 +5,7 @@ import requests
 import re
 import os,sys
 
-path=os.path.dirname(os.path.dirname(server1.txt))
+path=os.path.dirname(os.path.dirname("server1.txt"))
 txt="hah"
 fn=open(path,"w",encoding="utf-8")
 fn.write("zhongguo")
