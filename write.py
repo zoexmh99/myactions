b4a2d63a5be8526cf580a44596aa9b8518d0304f
lixiansheng828 <87464542+lixiansheng828@users.noreply.github.com>
2022-05-28 16:28:33 +0800

    Update write.py

--- write.py
+++ write.py
@@ -6,8 +6,8 @@ import re
 import os
 
 txt="hah"
-fn=open("./server1.txt","w+",encoding="utf-8")
-print("./server1.txt"+fn.readline())
+fn=open("server1.txt","r+",encoding="utf-8")
+print("server1.txt"+fn.readline())
 
 fn.write(txt)
 
